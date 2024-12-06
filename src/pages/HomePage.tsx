e392f1d4470ed99921757e37a3bab15f10320c89
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-12-06 12:31:13 -0800

    refactored HomePage for daisyui and added validation for form input

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,6 +7,13 @@ const HomePage: React.FC = () => {
     const navigate = useNavigate();
 
     const handleSubmit = async () => {
+        if (file === null) {
+
+            return;
+        } else if (category === '') {
+            return;
+        }
+
         const formData = new FormData();
         formData.append('file', file!);
         formData.append('category', category);
@@ -21,12 +28,12 @@ const HomePage: React.FC = () => {
     };
 
     return (
-        <div className="max-w-lg mx-auto p-4 bg-gray-900 text-white">
-            <h1 className="text-3xl">Start New Audit</h1>
+        <div className="max-w-lg mx-auto p-4 bg-base-300 rounded-xl flex flex-col justify-center items-center gap-4">
+            <h1 className="text-3xl font-semibold">Start New Audit</h1>
             <select
                 value={category}
                 onChange={(e) => setCategory(e.target.value)}
-                className="block w-full mt-4 p-2 text-black bg-white border border-gray-400 rounded"
+                className="select select-bordered w-full max-w-xs"
             >
                 <option disabled value="">Select Category</option>
                 <option value="WCS Supplements">Supplements</option>
@@ -36,12 +43,12 @@ const HomePage: React.FC = () => {
             </select>
             <input
                 type="file"
+                className="file-input file-input-bordered w-full max-w-xs"
                 onChange={(e) => setFile(e.target.files![0])}
-                className="block w-full mt-4"
             />
             <button
                 onClick={handleSubmit}
-                className="mt-4 p-2 bg-blue-500 text-white rounded"
+                className="btn btn-primary"
             >
                 Upload CSV and Start Audit
             </button>
